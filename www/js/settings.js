aa20a0ab65e6c918ae622328e89914877270ba46
Julius Karanjah <jkaranjah@clemcreativity.com>
2016-12-13 17:53:21 +0300

    push to compile

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,3 +1,4 @@
 //var url = 'http://localhost/job/new/masterminD/public/';
+var url = 'http://localhost/job/new/uppublic/public/';
 //var url = 'http://sharpmyessay.com/mastermind/public/';
-var url = 'http://upmtk.azurewebsites.net/';
+//var url = 'http://upmtk.azurewebsites.net/';
